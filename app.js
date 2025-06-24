f891f61facab3a562b09418dc88d3264549729d0
jaegonlee <jaegonlee0@gmail.com>
2025-06-25 00:27:01 +0900

    add p5

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+import p5 from "p5";
+
+let p5sketch = (p) => {
+
+  p.setup = () => {
+    p.createCanvas(400, 400);
+    p.background(0);
+  }
+
+  p.draw = () => {
+    p.background(0);
+    p.ellipse(x, 20, 20, 20);
+  }
+};
+
+let myp5 = new p5(p5sketch);
+
 const express = require("express");
 
 const app = express();
